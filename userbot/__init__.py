3df789a04ff7654c0dc3ba8838b96442a01e0d89
Patel Harsh <76835687+Harsh-78@users.noreply.github.com>
2021-06-03 13:48:20 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -5,7 +5,7 @@ from telethon.sessions import StringSession
 from telethon import TelegramClient
 from userbot.helpers import functions as simpdef
 from userbot.Config import Config
-from var import Var
+
 
 StartTime = time.time()
 botversion = "0.1"
