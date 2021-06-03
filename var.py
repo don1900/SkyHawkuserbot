26aaa4076c8b3247bfe802bdf282e47c266415a2
don1900 <80756375+don1900@users.noreply.github.com>
2021-06-03 16:09:43 +0545

    Update var.py

--- var.py
+++ var.py
@@ -3,7 +3,7 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
-    from sample_config import Var as Config
+    from heoku_config import Var as Config
 else:
     if os.path.exists("config.py"):
         from local_config import Development as Config
