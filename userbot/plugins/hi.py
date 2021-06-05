5215793141179e49a482a7963e5aa386a3de4254
don1900 <80756375+don1900@users.noreply.github.com>
2021-06-05 13:04:55 +0545

    Update hi.py

--- userbot/plugins/hi.py
+++ userbot/plugins/hi.py
@@ -3,8 +3,9 @@ from SkyhawkBot.utils import admin_cmd
 import asyncio
 from telethon import events
 
-@borg.on(admin_cmd(pattern="hi ?(.*)")
+@borg.on(admin_cmd(pattern=r"hi$", outgoing=True))
 async def _(event):
+    if event.fwd_from:
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("""HI HOW ARE YOU""")
         await asyncio.sleep(1)
