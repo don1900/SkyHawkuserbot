412ad25c5fc61cd2f716635430b18cef594b6a3a
don1900 <80756375+don1900@users.noreply.github.com>
2021-06-05 13:44:43 +0545

    Create hi.py

---
+++ userbot/plugins/hi.py
@@ -0,0 +1,11 @@
+import random, re
+from SkyhawkBot.utils import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern=r"hi$", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        await event.edit("""HI HOW ARE YOU""")
+        await asyncio.sleep(1)
+        await event.edit("""'🌺✨✨🌺✨🌺🌺🌺/n🌺✨✨🌺✨✨🌺✨/n🌺🌺🌺🌺✨✨🌺✨/n🌺✨✨🌺✨✨🌺✨/n🌺✨✨🌺✨🌺🌺🌺/n☁️☁️☁️☁️☁️☁️☁️☁️""")
