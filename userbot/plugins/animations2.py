d4d2d892f14556a70f2958658a02434a1f7b0f85
don1900 <80756375+don1900@users.noreply.github.com>
2021-06-09 13:57:27 +0545

    Update animations2.py

--- userbot/plugins/animations2.py
+++ userbot/plugins/animations2.py
@@ -43,7 +43,7 @@ async def _(event):
     animation_ttl = range(12)
     event = await edit_or_reply(event, "`Deploying...`")
     animation_chars = [
-        "**Heroku Connecting To Latest [Github Build](SkyhawkNetwork/SkyhawkBot)**",
+        "**Heroku Connecting To Latest [Github Build](don1900/SkyhawkBot)**",
         f"**Build started by user** {DEFAULTUSER}",
         f"**Deploy** `535a74f0` **by user** **{DEFAULTUSER}**",
         "**Restarting Heroku Server...**",
