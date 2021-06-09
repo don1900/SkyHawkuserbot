504f83bfb6fac0ea88bd55522c10061e72817de2
don1900 <80756375+don1900@users.noreply.github.com>
2021-06-09 12:36:52 +0545

    Update bot_pms.py

--- userbot/plugins/assistant/bot_pms.py
+++ userbot/plugins/assistant/bot_pms.py
@@ -64,8 +64,7 @@ async def check_bot_started_users(user, event):
         await event.client.send_message(BOTLOG_CHATID, notification)
 
 
-@SkyHawkbot.bot_cmd(
-    pattern=f"^/start({botusername})?([\s]+)?$",
+@SkyHawkbot.bot_cmd(pattern="/start$")?([\s]+)?$",
     incoming=True,
     func=lambda e: e.is_private,
 )
