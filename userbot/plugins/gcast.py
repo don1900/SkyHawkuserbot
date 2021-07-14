efcb2e3169a24e525781fe60d7e6460b657d1050
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-07-14 12:47:39 +0530

    added gcast

---
+++ userbot/plugins/gcast.py
@@ -0,0 +1,22 @@
+# made for SkyhawkBot
+# credits - LEGENDX22 and PROBOYX22
+
+from ..utils import admin_cmd
+@bot.on(admin_cmd(pattern='gcast'))
+async def broadcast(event):
+  Pro = event.text.split(" ", 1)[1]
+  async for PROBOYX in bot.iter_dialogs():
+     if not PROBOYX.is_group:
+       continue
+     fail = 0
+     succ = 0
+     chat = PROBOYX.id
+     if PROBOYX.is_channel:
+       pass
+     try:
+        await bot.send_message(chat, Pro)
+        succ += 1
+     except:
+        fail += 1
+        pass
+  await event.edit(f' done = {succ}\n fail = {fail}')
