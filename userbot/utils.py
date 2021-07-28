e7765530f891f798d97cc93c113601f06941f402
don1900 <80756375+don1900@users.noreply.github.com>
2021-07-28 13:12:55 +0545

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -602,3 +602,11 @@ def command(**args):
         return func
 
     return decorator
+
+async def eor(event, text):
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to = await event.get_reply_message()
+        if reply_to:
+            return await reply_to.reply(text)
+        return await event.reply(text)
+    return await event.edit(text)
